aa7d4128f7796eb44a450d1c4df00fe4cddcb5ce
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-02 23:27:24 -0500

    Updated 'Person' module and tests: 'privatized' instance props. (now accessible via 'get()' instance method). Added new test.

--- spec/person.spec.js
+++ spec/person.spec.js
@@ -4,18 +4,27 @@ describe( 'Test "Person" Constructor', function() {
     it ( 'Should require an options object.', function() {
         var person = new Person();
 
-        expect( person.name ).toBe( 'John Doe' );
+        expect( person.get( 'name' ) ).toBe( 'John Doe' );
     } );
-    
+
+
     it ( 'Should be anonymous if no name was provided.', function() {
         var person = new Person( { age: 27, sex: 'male' } );
 
-        expect( person.name ).toBe( 'John Doe' );
+        expect( person.get( 'name' ) ).toBe( 'John Doe' );
     } );
+
 
     it ( 'Should return the name provided at initialization.', function() {
         var person = new Person( { name: 'Jesse R Mykolyn' } );
 
-        expect( person.name ).toBe( 'Jesse R Mykolyn' );
+        expect( person.get( 'name' ) ).toBe( 'Jesse R Mykolyn' );
+    } );
+
+
+    it ( 'Should return a "false-y" value if the `name` property is accessed directly.', function() {
+        var person = new Person( { name: 'Jesse R Mykolyn' } );
+
+        expect( person.name ).not.toBeTruthy();
     } );
 } );
